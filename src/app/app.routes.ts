892397ab689dad5ad726d89ee9f9e8e4f0087cdc
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2025-02-27 19:17:04 +0530

    componentupdate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BookingComponent } from './components/booking/booking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+           { path: 'dashboard',
+            component: DashboardComponent},
+
+            { path: 'customer',
+                component: CustomerComponent},
+
+                { path: 'vehicle',
+                    component: VehicleComponent},
+
+                    { path: 'booking',
+                        component:BookingComponent},
+         ]
+
+    }
+];
